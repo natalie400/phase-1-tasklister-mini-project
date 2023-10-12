613f5ef088d022ea807ba7a43c92e5721ddab95a
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-12 20:47:03 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildList(e.target.newTaskDescription.value);
+    form.reset()
+  });
+  
+  function buildList(todo) {
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+    btn.textContent = 'x'; 
+    btn.addEventListener('click', toDelete)
+    p.textContent = `${todo} `;
+    p.appendChild(btn);
+    console.log(p);
+    document.querySelector('#list').appendChild(p);
+  }
+  function toDelete(e){
+    e.target.parentNode.remove()
+  }
 });
+
